cd6178d3ade9c1582c4adaec8aebbb8807539697
Fernando Aleman <fernandoaleman@mac.com>
2011-06-27 20:58:38 -0500

    Installed nifty layout with Haml and setup devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,5 +22,8 @@ Sharebox::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
+  
+  # Setup default url options
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
 
