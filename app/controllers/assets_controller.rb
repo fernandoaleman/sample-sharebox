2f41243f479b07034f1141f21fd1b27f5ef24be0
Fernando Aleman <fernandoaleman@mac.com>
2011-06-28 21:38:46 -0500

    Added user auth for files

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -1,31 +1,26 @@
 class AssetsController < ApplicationController
-  def index
-    @assets = Asset.all
-  end
 
-  def show
-    @asset = Asset.find(params[:id])
+  before_filter :authenticate_user!
+  
+  def index
+    @assets = current_user.assets
   end
 
   def new
-    @asset = Asset.new
+    @asset = current_user.assets.new
   end
 
   def create
-    @asset = Asset.new(params[:asset])
+    @asset = current_user.assets.new(params[:asset])
     if @asset.save
-      redirect_to @asset, :notice => "Successfully created asset."
+      redirect_to root_url, :notice => "Successfully created asset."
     else
       render :action => 'new'
     end
   end
 
-  def edit
-    @asset = Asset.find(params[:id])
-  end
-
   def update
-    @asset = Asset.find(params[:id])
+    @asset = current_user.assets.find(params[:id])
     if @asset.update_attributes(params[:asset])
       redirect_to @asset, :notice  => "Successfully updated asset."
     else
@@ -34,8 +29,18 @@ class AssetsController < ApplicationController
   end
 
   def destroy
-    @asset = Asset.find(params[:id])
+    @asset = current_user.assets.find(params[:id])
     @asset.destroy
     redirect_to assets_url, :notice => "Successfully destroyed asset."
+  end
+  
+  def get
+    asset = current_user.assets.find_by_id(params[:id])
+    if asset
+        send_file asset.uploaded_file.path, :type => asset.uploaded_file_content_type
+    else
+        flash[:error] = "Don't be cheeky! Mind your own assets!"
+        redirect_to assets_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Sharebox::Application.routes.draw do
   devise_for :users
 
   root :to => "home#index"
+  
+  match "assets/get/:id" => "assets#get", :as => "download"
 end
