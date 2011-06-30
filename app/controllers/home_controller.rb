d5e010645de08342fc617561e4d575eadf0a0d67
Fernando Aleman <fernandoaleman@mac.com>
2011-06-29 23:52:25 -0500

    Added ability to display folders on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,10 @@ class HomeController < ApplicationController
 
   def index
     if user_signed_in?
+      # load current user's folders
+      @folders = current_user.folders.order("name asc")
+      
+      # load current user's files(assets)
       @assets = current_user.assets.order("uploaded_file_file_name asc")
     end
   end
