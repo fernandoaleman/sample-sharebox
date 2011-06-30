9c462a4dee5f33cc90f8462d8e52409a37f0ce84
Fernando Aleman <fernandoaleman@mac.com>
2011-06-29 22:04:34 -0500

    Added files to the home page and fixed dowload links

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
+
   def index
+    if user_signed_in?
+      @assets = current_user.assets.order("uploaded_file_file_name asc")
+    end
   end
 
 end
--- app/models/asset.rb
+++ app/models/asset.rb
@@ -5,15 +5,17 @@ class Asset < ActiveRecord::Base
   
   #set up "uploaded_file" field as attached_file (using Paperclip)
   has_attached_file :uploaded_file,
-                    :path => "assets/:id/:basename.:extension",
-                    :storage => :s3,
-                    :s3_credentials => "#{RAILS_ROOT}/config/amazon_s3.yml",
-                    :bucket => "sample-sharebox"
+                    :url => "/assets/get/:id",
+                    :path => ":Rails_root/assets/:id/:basename.:extension"
 
   validates_attachment_size :uploaded_file, :less_than => 10.megabytes  
   validates_attachment_presence :uploaded_file
   
   def file_name
     uploaded_file_file_name
+  end
+  
+  def file_size
+    uploaded_file_file_size
   end
 end
